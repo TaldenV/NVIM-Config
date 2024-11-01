f1d7483d3eb12af306e47d8ee4ea438d43c322ed
TJ VanderLouw <taldenv@gmail.com>
2024-11-01 07:42:48 -0600

    add write quit to leader

--- init.lua
+++ init.lua
@@ -168,8 +168,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Set write/quit
+vim.keymap.set('n', '<leader>q', ':q!<Cr>', { desc = '[Q]uit' })
+vim.keymap.set('n', '<leader>ww', ':w!<Cr>', { desc = '[W]rite' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
